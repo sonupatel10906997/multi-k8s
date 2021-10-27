57f1595be72a360594a657e0cd6ecb06acd4fc2a
Sonu Patel <sonupatel10906997@gmail.com>
2021-10-27 14:04:44 -0500

    fix spelling mistakes in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,16 @@ docker build -t sonupatel10906997/multi-client:latest -t sonupatel10906997/multi
 docker build -t sonupatel10906997/multi-server:latest -t sonupatel10906997/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t sonupatel10906997/multi-worker:latest -t sonupatel10906997/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push sonupatel10906997/mutli-client:latest
-docker push sonupatel10906997/mutli-worker:latest
+docker push sonupatel10906997/multi-client:latest
+docker push sonupatel10906997/multi-worker:latest
 docker push sonupatel10906997/multi-server:latest
 
-docker push sonupatel10906997/mutli-client:$SHA
-docker push sonupatel10906997/mutli-worker:$SHA
+docker push sonupatel10906997/multi-client:$SHA
+docker push sonupatel10906997/multi-worker:$SHA
 docker push sonupatel10906997/multi-server:$SHA
 
 kubectl apply -f k8s
 
 kubectl set image deployments/server-deployment server=sonupatel10906997/multi-server:$SHA
-kubectl set image deployment/client-deployment client=sonupatel10906997/mutli-client:$SHA
+kubectl set image deployment/client-deployment client=sonupatel10906997/multi-client:$SHA
 kubectl set image deployment/worker-deployment worker=sonupatel10906997/multi-worker:$SHA
